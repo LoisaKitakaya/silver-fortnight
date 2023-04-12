a656e71e6349d7014f1d0db17365e3ce66e90731
Freedom Loisa <kitakayaloisa@gmail.com>
2023-04-12 10:14:44 +0300

    registering blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,46 @@ from flask import Flask
 
 from config import Config
 
+# app blueprints
+
+from app.algebra import bp as algebra_bp
+from app.arithmetics import bp as arithmetics_bp
+from app.calculus import bp as calculus_bp
+from app.finance import bp as finance_bp
+from app.geometry import bp as geometry_bp
+from app.logic import bp as logic_bp
+from app.matrices import bp as matrices_bp
+from app.numerics import bp as numerics_bp
+from app.statistics import bp as statistics_bp
+from app.trigonometry import bp as trigonometry_bp
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize Flask extensions here
+    # Flask extensions
 
-    # Register blueprints here
+    # app blueprints
 
-    @app.route("/test/")
-    def test_page():
-        return "<h1>Testing the Flask Application Factory Pattern</h1>"
+    app.register_blueprint(algebra_bp)
+
+    app.register_blueprint(arithmetics_bp)
+
+    app.register_blueprint(calculus_bp)
+
+    app.register_blueprint(finance_bp)
+
+    app.register_blueprint(geometry_bp)
+
+    app.register_blueprint(logic_bp)
+
+    app.register_blueprint(matrices_bp)
+
+    app.register_blueprint(numerics_bp)
+
+    app.register_blueprint(statistics_bp)
+
+    app.register_blueprint(trigonometry_bp)
 
     return app
